0fa6df68b9cc6c90425c9ecccd24ffe54cf78192
Matias Senger <m.senger@hotmail.com>
2021-04-02 20:59:47 +0200

    First full implementation of popups in cross references

--- css_and_scripts/script.js
+++ css_and_scripts/script.js
@@ -6,7 +6,8 @@ try {
 
 // Parse <figure> tags -------------------------------------------------
 var figures = document.getElementById("document-content").getElementsByTagName("figure");
-var figures_reference_texts = {}
+var figures_reference_texts = {};
+var figures_reference_popup_texts = {};
 for(var i = 0; i < figures.length; i++) {
 	var caption = figures[i].getElementsByTagName("figcaption")[0];
 	caption.innerHTML = `<b>Figure ${i+1}. </b>` + caption.innerHTML;
@@ -18,6 +19,7 @@ for(var i = 0; i < figures.length; i++) {
 		throw `ERROR: The id "${figures[i].id}" is used in more than one figure in the document. ids cannot repeat, please fix this.`;
 	}
 	figures_reference_texts[figures[i].id] = `${i+1}`; // This text will be placed where there is a reference to this figure.
+	figures_reference_popup_texts[figures[i].id] = caption.innerHTML;
 }
 // Parse <equation> tags -----------------------------------------------
 var equations = document.getElementById("document-content").getElementsByTagName("equation");
@@ -58,6 +60,7 @@ function get_all_numbered_h(document) {
 }
 var numbered_headings = get_all_numbered_h(document);
 var headings_reference_texts = {};
+var headings_reference_popup_texts = {};
 var current_section_numbering = [0];
 for (var i=0; i<numbered_headings.length; i++) {
 	var current_indentation_level = parseInt(numbered_headings[i].tagName.toLowerCase().replace('h', ''));
@@ -82,6 +85,7 @@ for (var i=0; i<numbered_headings.length; i++) {
 		throw `ERROR: The id/name "${current_id}" is used in more than one heading in the document. If you manually defined this id please change one of them as they cannot repeat, if you have more than one section with the same name please assign them different ids to the section in order to use the same title.`;
 	}
 	headings_reference_texts[current_id] = current_section_numbering.join('.');
+	headings_reference_popup_texts[current_id] = numbered_headings[i].innerHTML;
 	numbered_headings[i].id = current_id; // If the heading had no id, this will set it. Otherwise it does nothing.
 }
 // Automatic table of contents -----------------------------------------
@@ -133,7 +137,7 @@ if (document.getElementById("table-of-contents") != null) {
 // Parse <crossref> tags -----------------------------------------------
 var crossref = document.getElementById("document-content").getElementsByTagName("crossref");
 const texts_for_cross_references_by_id = Object.assign({}, figures_reference_texts, equations_reference_texts, headings_reference_texts); // This is a dictionary of the form dict[id] = "text_to_be_shown_in_the_reference".
-const texts_for_popup_windows_by_id = Object.assign({}, equations_reference_popup_texts);
+const texts_for_popup_windows_by_id = Object.assign({}, equations_reference_popup_texts, headings_reference_popup_texts, figures_reference_popup_texts);
 for(var i = 0; i < crossref.length; i++) {
 	var ref_to_this_id = crossref[i].innerHTML;
 	var reference_str;
