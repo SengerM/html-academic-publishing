b93c25c1e7f8f030de69f251dcbd13d6e80fae32
Matias Senger <m.senger@hotmail.com>
2021-04-05 18:53:41 +0200

    First implementation of authors with affiliations

--- css_and_scripts/script.js
+++ css_and_scripts/script.js
@@ -4,6 +4,42 @@ try {
 	document.getElementById("document_title").innerHTML = document.title;
 } catch {}
 
+// Parse <author> tags -------------------------------------------------
+var authors = document.getElementsByTagName('author');
+if (authors.length > 0) {
+	var affiliations_list = new Set();
+	for (var i=0; i<authors.length; i++) {
+		var current_author_affiliations = authors[i].getAttribute('affiliation').split(',');
+		for (var k=0; k<current_author_affiliations.length; k++)
+			affiliations_list.add(current_author_affiliations[k]);
+	}
+	affiliations_list = Array.from(affiliations_list);
+	affiliations_list.sort();
+	var authors_names_container = document.createElement('div');
+	authors_names_container.setAttribute('id', 'authors_names_container');
+	authors[0].parentNode.insertBefore(authors_names_container, authors[0]);
+	for (var i=0; i<authors.length; i++) {
+		authors_names_container.appendChild(authors[i]);
+		var current_author_affiliations = authors[i].getAttribute('affiliation').split(',');
+		var current_author_affiliations_text_with_references = '';
+		for (var k=0; k<affiliations_list.length; k++) {
+			if (current_author_affiliations.indexOf(affiliations_list[k]) > -1)
+				current_author_affiliations_text_with_references += `${k+1}`;
+			if (k<current_author_affiliations.length-1)
+				current_author_affiliations_text_with_references += ',';
+		}
+		authors[i].innerHTML += '<sup>' + current_author_affiliations_text_with_references + '</sup>';
+	}
+	var authors_affiliations_container = document.createElement('div');
+	authors_affiliations_container.setAttribute('id', 'authors_affiliations_container');
+	authors_names_container.parentNode.insertBefore(authors_affiliations_container, authors_names_container.nextSibling);
+	for (var i=0; i<affiliations_list.length; i++) {
+		var current_affiliation_element = document.createElement('div');
+		current_affiliation_element.setAttribute('class', 'author_affiliation');
+		authors_affiliations_container.appendChild(current_affiliation_element);
+		current_affiliation_element.innerHTML = `<sup>${i+1}</sup>${affiliations_list[i]}`;
+	}
+}
 // Parse <figure> tags -------------------------------------------------
 var figures = document.getElementsByTagName("figure");
 var figures_reference_texts = {};
