a29bca12ce65ecfdeb6eaf48b9b3091a81c70fd1
Matias Senger <m.senger@hotmail.com>
2021-05-02 11:33:57 +0200

    First implementation of presentation_display_block, or "slides"

---
+++ css_and_scripts/presentation_display_block/presentation_display_block.js
@@ -0,0 +1,49 @@
+var presentation_display_blocks = document.getElementsByClassName("presentation_display_block");
+var last_shown_presentation_display_block = 0;
+for (var i=0; i<presentation_display_blocks.length; i++) {
+	presentation_display_blocks[i].id = `presentation_display_block_${i}`;
+	if (i>0)
+		presentation_display_blocks[i].classList.add("presentation_display_block_hidden");
+}
+
+var show_next_button = document.createElement('button');
+show_next_button.id = 'show_next_button';
+show_next_button.innerHTML = 'Next';
+show_next_button.setAttribute('onClick', 'showNextSlideBlock()');
+
+var show_all_button = document.createElement('button');
+show_all_button.id = 'show_all_button';
+show_all_button.innerHTML = 'Show all remaining';
+show_all_button.setAttribute('onClick', 'showAllSlideBlocks()');
+
+var buttons_at_the_bottom_container = document.createElement('div');
+buttons_at_the_bottom_container.id = 'buttons_at_the_bottom_container';
+presentation_display_blocks[last_shown_presentation_display_block].parentNode.insertBefore(buttons_at_the_bottom_container, presentation_display_blocks[last_shown_presentation_display_block].nextSibling);
+buttons_at_the_bottom_container.appendChild(show_next_button);
+buttons_at_the_bottom_container.appendChild(show_all_button);
+
+function showNextSlideBlock() {
+	if (last_shown_presentation_display_block < presentation_display_blocks.length -1) {
+		last_shown_presentation_display_block += 1;
+		presentation_display_blocks[last_shown_presentation_display_block].classList.remove('presentation_display_block_hidden');
+		presentation_display_blocks[last_shown_presentation_display_block].parentNode.insertBefore(buttons_at_the_bottom_container, presentation_display_blocks[last_shown_presentation_display_block].nextSibling);
+		presentation_display_blocks[last_shown_presentation_display_block].scrollIntoView();
+	}
+	if (last_shown_presentation_display_block == presentation_display_blocks.length - 1) {
+		show_next_button.remove();
+		show_all_button.remove();
+	}
+}
+
+async function showAllSlideBlocks() {
+	const current_presentation_display_block = last_shown_presentation_display_block;
+	function sleep(ms) {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	}
+	while (last_shown_presentation_display_block < presentation_display_blocks.length-1) {
+		showNextSlideBlock();
+		await sleep(1111/presentation_display_blocks.length);
+	}
+	await sleep(777);
+	presentation_display_blocks[current_presentation_display_block].scrollIntoView();
+}
