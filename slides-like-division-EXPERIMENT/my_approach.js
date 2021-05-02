992a4492bc293ce31cedb51604eaf2eca63b3783
Matias Senger <m.senger@hotmail.com>
2021-05-02 02:10:47 +0200

    Improved positioning of navigation buttons

--- slides-like-division-EXPERIMENT/my_approach.js
+++ slides-like-division-EXPERIMENT/my_approach.js
@@ -6,28 +6,27 @@ for (var i=0; i<slide_blocks.length; i++) {
 		slide_blocks[i].classList.add("slide_block_hidden");
 }
 
-//~ var buttons_at_the_bottom_container = document.createElement('div');
-//~ buttons_at_the_bottom_container.id = 'buttons_at_the_bottom_container';
-//~ slide_blocks[last_shown_slide_block].parentNode.insertBefore(buttons_at_the_bottom_container, slide_blocks[last_shown_slide_block].nextSibling);
-
-
 var show_next_button = document.createElement('button');
 show_next_button.id = 'show_next_button';
 show_next_button.innerHTML = 'Next';
 show_next_button.setAttribute('onClick', 'showNextSlideBlock()');
-slide_blocks[last_shown_slide_block].parentNode.insertBefore(show_next_button, slide_blocks[last_shown_slide_block].nextSibling);
 
 var show_all_button = document.createElement('button');
 show_all_button.id = 'show_all_button';
 show_all_button.innerHTML = 'Show all';
 show_all_button.setAttribute('onClick', 'showAllSlideBlocks()');
-document.body.insertBefore(show_all_button, document.body.firstChild);
+
+var buttons_at_the_bottom_container = document.createElement('div');
+buttons_at_the_bottom_container.id = 'buttons_at_the_bottom_container';
+slide_blocks[last_shown_slide_block].parentNode.insertBefore(buttons_at_the_bottom_container, slide_blocks[last_shown_slide_block].nextSibling);
+buttons_at_the_bottom_container.appendChild(show_next_button);
+buttons_at_the_bottom_container.appendChild(show_all_button);
 
 function showNextSlideBlock() {
 	if (last_shown_slide_block < slide_blocks.length -1) {
 		last_shown_slide_block += 1;
 		slide_blocks[last_shown_slide_block].classList.remove('slide_block_hidden');
-		slide_blocks[last_shown_slide_block].parentNode.insertBefore(show_next_button, slide_blocks[last_shown_slide_block].nextSibling);
+		slide_blocks[last_shown_slide_block].parentNode.insertBefore(buttons_at_the_bottom_container, slide_blocks[last_shown_slide_block].nextSibling);
 		slide_blocks[last_shown_slide_block].scrollIntoView();
 	}
 	if (last_shown_slide_block == slide_blocks.length - 1) {
